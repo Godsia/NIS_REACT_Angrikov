3037abdf6fe1b6ee0e1a487c8a72b7a765894d56
Godsia <151738440+Godsia@users.noreply.github.com>
2025-10-24 18:14:22 +0300

    test 1

---
+++ src/data/movieData/index.ts
@@ -0,0 +1,74 @@
+import {IFilm} from "../../common/types";
+
+export const moviesData: IFilm[] = [
+    {
+        id: 1,
+        title: "Мстители",
+        year: 2012,
+        posterUrl: "https://m.media-amazon.com/images/I/81ExhpBEbHL._AC_SY679_.jpg",
+        isFavorite: true,
+    },
+    {
+        id: 2,
+        title: "Мстители: Эра Альтрона",
+        year: 2015,
+        posterUrl: "https://m.media-amazon.com/images/I/71qQfQpM6wL._AC_SY679_.jpg",
+        isFavorite: false,
+    },
+    {
+        id: 3,
+        title: "Мстители: Война бесконечности",
+        year: 2018,
+        posterUrl: "https://m.media-amazon.com/images/I/91jvOj8YkKL._AC_SY679_.jpg",
+        isFavorite: true,
+    },
+    {
+        id: 4,
+        title: "Мстители: Финал",
+        year: 2019,
+        posterUrl: "https://m.media-amazon.com/images/I/81ai6zx6eXL._AC_SY679_.jpg",
+        isFavorite: false,
+    },
+    {
+        id: 5,
+        title: "Человек-паук: Нет пути домой",
+        year: 2021,
+        posterUrl: "https://m.media-amazon.com/images/I/71niXI3lxlL._AC_SY679_.jpg",
+        isFavorite: true,
+    },
+    {
+        id: 6,
+        title: "Доктор Стрэндж",
+        year: 2016,
+        posterUrl: "https://m.media-amazon.com/images/I/81qvoeV+HCL._AC_SY679_.jpg",
+        isFavorite: false,
+    },
+    {
+        id: 7,
+        title: "Черная Пантера",
+        year: 2018,
+        posterUrl: "https://m.media-amazon.com/images/I/81Qxk8UCrML._AC_SY679_.jpg",
+        isFavorite: true,
+    },
+    {
+        id: 8,
+        title: "Тор: Рагнарёк",
+        year: 2017,
+        posterUrl: "https://m.media-amazon.com/images/I/81fA3n0T9-L._AC_SY679_.jpg",
+        isFavorite: false,
+    },
+    {
+        id: 9,
+        title: "Стражи Галактики",
+        year: 2014,
+        posterUrl: "https://m.media-amazon.com/images/I/91eOgodm+tL._AC_SY679_.jpg",
+        isFavorite: true,
+    },
+    {
+        id: 10,
+        title: "Железный человек",
+        year: 2008,
+        posterUrl: "https://m.media-amazon.com/images/I/91I5UHVjHVL._AC_SY679_.jpg",
+        isFavorite: false,
+    },
+];
